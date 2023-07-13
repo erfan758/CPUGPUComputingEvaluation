8adc2ec0e00510a8444e2b5cc0fcb591ecbe753c
erfan758 <113509080+erfan758@users.noreply.github.com>
2023-07-13 07:55:23 +0330

    Update Scenario1.py

--- Scenario1.py
+++ Scenario1.py
@@ -1,4 +1,4 @@
-# ************************ Written by Alireza ************************************
+
 import numpy as np
 import time
 import torch
@@ -50,25 +50,3 @@ print(gpu_result)
 print("CPU Execution Time:", cpu_execution_time, "seconds")
 print("GPU Execution Time:", gpu_execution_time, "seconds")
 
-""" The Explanation of the Code:
-In this code, we start by defining the size of the square matrices to be multiplied (matrix_size). 
-We then generate two random matrices (matrix_a and matrix_b) using the np.random.rand function.
-
-We define two functions: cpu_matrix_multiplication and gpu_matrix_multiplication, 
-which perform matrix multiplication using CPU and GPU, respectively. 
-
-In the CPU function, we use the NumPy np.dot function for matrix multiplication. 
-In the GPU function, we use the cp.asarray function from the cupy library 
-to transfer the matrices to the GPU, perform matrix multiplication using cp.dot, 
-and then transfer the result back to the CPU using cp.asnumpy.
-
-After running the CPU and GPU matrix multiplication functions, 
-we can optionally compare the results and print the execution times.
-
-Please note that for the GPU matrix multiplication to work, 
-you need to have the cupy library installed, which provides 
-a NumPy-compatible interface for GPU computations. 
-Also, make sure you have a compatible GPU and CUDA drivers installed.
-
-Feel free to adjust the matrix size and modify the code according to your specific requirements.
-"""
